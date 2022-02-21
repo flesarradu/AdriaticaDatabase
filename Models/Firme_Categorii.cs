3e1830dd21bf7881476c45fca2c0e9ececb5e103
romaniulian <iulian.roman01@e-uvt.ro>
2022-02-21 23:57:11 +0200

    inca 2 clase

---
+++ Models/Firme_Categorii.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdriaticaDatabase.Models
+{
+    public class Firme_Categorii
+    {
+        int IdCategorieFirma { get; set; }
+        int IdFirmaCF { get; set; }
+        Firme FirmaCF { get; set; }
+        int IdCategorieCF { get; set; }
+        Lista_Categorii_Firme CategorieCF { get; set; }
+    }
+}
